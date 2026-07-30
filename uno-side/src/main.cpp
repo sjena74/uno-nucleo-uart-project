2a675cc20b93e08b08148822eb9846ba9e38c63b
Sohan <jenasohan@gmail.com>
2026-07-29 20:38:50 -0400

    all sensors integrated into uno. Moving onto actuators next.

--- uno-side/src/main.cpp
+++ uno-side/src/main.cpp
@@ -2,15 +2,26 @@
 #include <HCSR04.h>
 
 HCSR04 hc(2, 3);
-
+const int PIR_PIN = 4;
+const int PHOTO_PIN = A0;
 void setup() {
   Serial.begin(9600);
+  pinMode(PIR_PIN, INPUT);
 }
 
 void loop() {
   Serial.print("Distance: ");
-  Serial.print(hc.dist());
+  Serial.print(hc.dist()); // method returns distance in cm
   Serial.println(" cm");
-  delay(60);
+
+  int motion = digitalRead(PIR_PIN); // returns HIGH/LOW
+  Serial.print("Motion: ");
+  Serial.println(motion == HIGH ? "detected" : "not detected");
+
+  int light = analogRead(PHOTO_PIN);
+  Serial.print("Light: ");
+  Serial.println(light);
+
+  delay(500);
 }
 
